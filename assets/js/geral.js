941f2ef657bbfa7a8356ee14c43d2aeb6808bb02
luiz-brancher <lppbrancher@senacrs.com.br>
2022-10-25 17:32:34 -0300

    aaaaaaaaaaaaaaa

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -2,8 +2,17 @@ window.addEventListener('load', function(){
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl)
+      console.log('?!?!?!')
     })
-
+    
     $('.preloader').fadeOut(300);
 })
-
+$('.tooltip-link').click(function(e){e.preventDefault()})
+$('.cartaMemoria').click(function(){
+  if($(this).hasClass('flip')){
+    $(this).removeClass('flip')  
+  } else {
+    $(this).addClass('flip')
+  }
+  
+})
